600dce1f972d5b159cbd4cf883c84c394809a557
A.O <35069360+oulfr@users.noreply.github.com>
2024-02-19 14:27:38 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install.sh
+bash hst-install.sh --multiphp yes --mysql8 yes --postgresql yes --sieve yes --quota yes --force
