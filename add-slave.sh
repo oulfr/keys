b907c94e1bebe3bc67dd086039e38d5d50de5ba6
A.O <35069360+oulfr@users.noreply.github.com>
2024-02-19 11:56:34 +0100

    Update add-slave.sh

--- add-slave.sh
+++ add-slave.sh
@@ -1,2 +1,2 @@
-v-add-remote-dns-host 10.27.27.203 8083 'ppVSTTm58cZBL7eQx33C:ddGsL8tjXVEKmgSdfdpuwQjw=ff_5w8cCAytgAbF' '' 'api' 'dns-sync-user'
-v-add-remote-dns-host 10.27.27.202 8083 'O7WBViN1eYUcEbHnsovp:mgJY73m535GdHf9oQ7Hc5rtZVjDpubI9_fPC7VuK' '' 'api' 'dns-sync-user'
+v-add-remote-dns-host 10.27.27.202 8083 'ppVSTTm58cZBL7eQx33C:ddGsL8tjXVEKmgSdfdpuwQjw=ff_5w8cCAytgAbF' '' 'api' 'dns-sync-user'
+v-add-remote-dns-host 10.27.27.203 8083 'O7WBViN1eYUcEbHnsovp:mgJY73m535GdHf9oQ7Hc5rtZVjDpubI9_fPC7VuK' '' 'api' 'dns-sync-user'
