67a19bec778ccc0f479305976221db7335f1198e
A.O <35069360+oulfr@users.noreply.github.com>
2024-02-25 19:36:15 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-bash hst-install-ubuntu.sh --hostname demo.hestiacp.com --email admin@example.com --username admin --password Password123 --with-debs /tmp/hestiacp-src/deb/ --interactive no --force
+bash hst-install-debian.sh --hostname demo.hestiacp.com --email admin@example.com --username admin --password Password123 --with-debs /tmp/hestiacp-src/deb/ --interactive no --force
